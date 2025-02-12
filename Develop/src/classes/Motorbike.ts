4976b8c3e36a99a4a534ff8bdbe06d99020b8284
Pricilla Francis <Pricilla@Pricillas-MacBook-Air.local>
2025-02-11 22:04:53 -0500

    update1

--- Develop/src/classes/Motorbike.ts
+++ Develop/src/classes/Motorbike.ts
@@ -3,7 +3,7 @@ import Vehicle from './Vehicle.js';
 import Wheel from './Wheel.js';
 
 // TODO: The Motorbike class should extend the Vehicle class
-class Motorbike {
+class Motorbike extends Vehicle {
   // TODO: Declare properties of the Motorbike class
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
@@ -17,57 +17,59 @@ class Motorbike {
   wheels: Wheel[];
 
   // TODO: Create a constructor that accepts the properties of the Motorbike class
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
-    // TODO: The constructor should initialize the properties of the Motorbike class
-    // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
-    constructor(
-      vin: string,
-      color: string,
-      make: string,
-      model: string,
-      year: number,
-      weight: number,
-      topSpeed: number,
-      wheels: Wheel[]
-    ) {
-      super ();
-      this.vin = vin;
-      this.color = color;
-      this.make = make;
-      this.model = model;
-      this.year = year;
-      this.weight = weight;
-      this.topSpeed = topSpeed;
+  // TODO: The constructor should call the constructor of the parent class, Vehicle
+  // TODO: The constructor should initialize the properties of the Motorbike class
+  // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[]
+  ) {
+    super();
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
 
-      if (wheels.length !== 2) {
-        this.wheels = [new Wheel(), new Wheel()];
-      } else {
-        this.wheels = wheels;
-      }
-  // TODO: Implement the wheelie method
-wheelie(): void {
-    // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
-    console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
-  // TODO: Override the printDetails method from the Vehicle class
-    override printDetails(): void {
-  // TODO: The method should call the printDetails method of the parent class
-      super.printDetails();
-  // TODO: The method should log the details of the Motorbike
-  // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+    if (wheels.length !== 2) {
+      this.wheels = [new Wheel(), new Wheel()];
+    } else {
+      this.wheels = wheels;
+    } }
 
-      console.log(`VIN: ${this.vin}`);
-      console.log(`Make: ${this.make}`);
-      console.log(`Model: ${this.model}`);
-      console.log(`Year: ${this.year}`);
-      console.log(`Weight: ${this.weight}`);
-      console.log(`Top Speed: ${this.topSpeed}`);
-      console.log(`Color: ${this.color}`);
-      console.log(
-        `Wheels: ${this.wheels[0].size} inches, ${this.wheels[0].make}, ${this.wheels[1].size} inches, ${this.wheels[1].make}`
-      );
+    // TODO: Implement the wheelie method
+    wheelie(): void {
+      // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
+      console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
     }
-  } 
+      // TODO: Override the printDetails method from the Vehicle class
+      override printDetails(): void {
+        // TODO: The method should call the printDetails method of the parent class
+        super.printDetails();
+        // TODO: The method should log the details of the Motorbike
+        // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
 
+        console.log(`VIN: ${this.vin}`);
+        console.log(`Make: ${this.make}`);
+        console.log(`Model: ${this.model}`);
+        console.log(`Year: ${this.year}`);
+        console.log(`Weight: ${this.weight}`);
+        console.log(`Top Speed: ${this.topSpeed}`);
+        console.log(`Color: ${this.color}`);
+        console.log(
+          `Wheels: ${this.wheels[0].getDiameter} inches, ${this.wheels[0].getTireBrand}, ${this.wheels[1].getDiameter} inches, ${this.wheels[1].getTireBrand}`
+        );
+      }
+    }
+  
 
-// Export the Motorbike class as the default export
-export default Motorbike;
+    // Export the Motorbike class as the default export
+    export default Motorbike;
